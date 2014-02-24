16923010d379216e0ff53336892b78d3118af752
John Gold <johnnyg12791@gmail.com>
2014-02-23 22:24:27 -0800

    adding parallel corpus and start to other file/functions

--- code/dictionary.py
+++ code/dictionary.py
@@ -1,15 +1,20 @@
-from nltk_tools import NltkTools
+from nltk_tools import *
 
 
 def main():
   stemmer = NltkTools()
-  filename = "../corpus/dict_unaccented.txt"
-  print create_dictionary(filename, stemmer)
+  word = "El movimiento del voto en Blanco encabezado por".decode('quopri').decode('utf-8')
+  print stemmer.spanish_unigram_tag(word)
+  raw_input("")
+  #filename = "../corpus/dict_unaccented.txt"
+  #print create_dictionary(filename, stemmer)
 
-#given a file of the form:
-#spanish_word: englishWord1, englishWord2...
-#spanish_word: englishWord1, englishWord2, englishWord3...
-#returns a dictionary {spanWord -> [engW, engW], spanWord -> [engW, engW,...]}
+'''
+given a file of the form:
+spanish_word: englishWord1, englishWord2...
+spanish_word: englishWord1, englishWord2, englishWord3...
+returns a dictionary {spanWord -> [engW, engW], spanWord -> [engW, engW,...]}
+'''
 def create_dictionary(filename, stemmer):
   dictionary = {}
   with open(filename) as f:
---
+++ code/sentence_alignment.py
@@ -0,0 +1,14 @@
+#Setence Alignments
+
+
+def main():
+  sentence_alignment(training_corpus)
+
+
+
+def sentence_alignment(training_corpus):
+  return "oops"
+  #continue until params converge
+
+if __name__ == '__main__':
+  main()
