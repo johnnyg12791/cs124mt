e6ddeb972d9052fb3f977d65329ff87614ba47fe
Harley Sugarman <harleysugarman@me.com>
2014-02-25 00:48:49 -0800

    added logprob english bigram dictionary

--- code/nltk_tools.py
+++ code/nltk_tools.py
@@ -7,8 +7,9 @@ import nltk
 A wrapper class provided useful implementations of several NLTK tools.
 
 Current tools include:
-- Spanish-language word stemmer
-- Spanish-language unigram/bigram POS tagger
+- Spanish-language word stemmer => stem_spanish_word(word)
+- Spanish-language unigram POS tagger => spanish_unigram_pos_tag(sentence)
+- Spanish-language bigram POS tagger => spanish_bigram_pos_tag(sentence)
 - English-language (not yet implemented)
 - Sentence/word normalizer (not yet implemented)
 
@@ -20,8 +21,10 @@ class NltkTools:
     corpus_dir = os.path.dirname(os.path.abspath(__file__)) + '/../corpus'
     nltk.data.path.append(corpus_dir)
     self.spanish_stemmer = nltk.SnowballStemmer("spanish")
-    self.unigram_tagger = self._load_tagger(corpus_dir + '/taggers/unigram.pickle', 'unigram')
-    self.bigram_tagger = self._load_tagger(corpus_dir + '/taggers/bigram.pickle', 'bigram')
+    self.es_unigram_tagger = self._load_tagger(corpus_dir + '/taggers/es_unigram.pickle', 'unigram')
+    self.es_bigram_tagger = self._load_tagger(corpus_dir + '/taggers/es_bigram.pickle', 'bigram')
+    self.en_unigram_model = self._load_model(corpus_dir + '/models/en_unigram.pickle', 1)
+    self.en_bigram_model = self._load_model(corpus_dir + '/models/en_bigram.pickle', 2)
    
   # Generate unigram and bigram word taggers, reading from 
   # pickled files if they already exist       
@@ -43,7 +46,19 @@ class NltkTools:
       pickle.dump(tagger, tagger_file, -1)
       tagger_file.close()
     return tagger
-      
+    
+  def _load_model(self, filename, ngram):
+    #model = None
+    #if os.path.isfile(filename):
+    #  model_file = open(filename, 'rb')
+    #  model = pickle.load(tagger_file)
+    #  model_file.close()
+    #else:
+    #  model_file = open(filename, 'wb')
+    model = nltk.model.ngram.NgramModel(ngram, nltk.corpus.brown.words())
+    #  pickle.dump(model, model_file, -1)
+    #  model_file.close()
+    return model
     
   # Returns a stemmed version of the input Spanish word
   def stem_spanish_word(self, word):
@@ -53,28 +68,28 @@ class NltkTools:
   # for each word based on a unigram language model
   def spanish_unigram_pos_tag(self, sentence):
     sentence = self.split_and_normalize_sentence(sentence)
-    return self.unigram_tagger.tag(sentence)
+    return self.es_unigram_tagger.tag(sentence)
     
   # Given a sentence to translate, this returns the POS
   # for each word based on a bigram language model
   def spanish_bigram_pos_tag(self, sentence):
     sentence = self.split_and_normalize_sentence(sentence)
-    return self.bigram_tagger.tag(sentence)
-  
-  ###############################################################
-  # Everything below this line is not yet finished. DO NOT USE! #
-  ###############################################################
+    return self.es_bigram_tagger.tag(sentence)
     
   # Given a unigram, return the probability of that
   # unigram occuring in the Brown corpus
   def english_unigram_probability(self, unigram):
-    pass
+    return self.en_unigram_model.logprob(unigram, [])
   
   # Given a bigram, return the probability of that
   # bigram occuring in the Brown corpus
   def english_bigram_probability(self, bigram):
-    pass
-  
+    return self.en_bigram_model.logprob(bigram[0], [bigram[1]])
+
+  ###############################################################
+  # Everything below this line is not yet finished. DO NOT USE! #
+  ###############################################################  
+
   def normalize_word(self, word):
     exclude = set(string.punctuation)
     word = ''.join(ch for ch in word if ch not in exclude)
