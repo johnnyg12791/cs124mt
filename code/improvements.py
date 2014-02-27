6b90e00a8e0a80c9eefa71540d93ef90f48ae168
Justin Salloum <jsalloum@stanford.edu>
2014-02-26 23:36:34 -0800

    fixing merge conflicts

--- code/improvements.py
+++ code/improvements.py
@@ -21,6 +21,8 @@ def main():
     content = f.readlines()
     for line in content:
       print unigram_sentences(nltk_tools, dictionary, line)
+      print 
+      break
 '''
   with open(dev_filename) as f:
     content = f.readlines()
@@ -30,27 +32,26 @@ def main():
 
 
 def unigram_sentences(nltk, dictionary, sentence):
+  #split sentence into words, while keeping whitespace, to make concatenation easier
   splitter = re.compile(r'(\s+|\S+)')
   words = splitter.findall(sentence)
+  
   result = ""
-
-
   for word in words:
-  #  if not word.isspace():
-  #   print word
-
+    #if we only have whitespace, just concatenate
     if word.isspace():
       result += word
+
     else:
       punc = ""
+      #check for punctuation, separatate punctuaion from word
       if (word[len(word) - 1] in string.punctuation):
         punc = word[len(word) - 1]
         word = word[:-1]
 
       stemmed_word = nltk.stem_spanish_word(word.decode('quopri').decode('utf-8'))
-
+      # if dictionary doesn't have stemmed word, then use closest match from dictionary
       if stemmed_word not in dictionary:
-        #print stemmed_word
         matches = difflib.get_close_matches(stemmed_word, dictionary.keys())
         if len(matches) != 0:
           result += translate.get_most_likely_definition(nltk, matches[0], dictionary) + punc
@@ -61,7 +62,7 @@ def unigram_sentences(nltk, dictionary, sentence):
   
   return result
 
-'''
+
 def bigram_sentences(nltk, dictionary, sentence):
   splitter = re.compile(r'(\s+|\S+)')
   words = splitter.findall(sentence)
@@ -84,7 +85,7 @@ def bigram_sentences(nltk, dictionary, sentence):
     if (word[len(word) - 1] in string.punctuation):
       punc = word[len(word) - 1]
       word = word[:-1]
-'''
+
 
 #helper function
 def get_best_unigram_word(nltk, meanings):
--- code/translate.py
+++ code/translate.py
@@ -2,6 +2,7 @@ import sys
 import os
 import nltk_tools
 from dictionary import *
+import difflib
 
 DEV_SET = os.path.dirname(os.path.abspath(__file__)) + '/../corpus/dev_set.txt'
 TEST_SET = os.path.dirname(os.path.abspath(__file__)) + '/../corpus/test_set.txt'
@@ -17,31 +18,219 @@ def main(args):
     content = f.readlines()
     for line in content:
       
-      print "Spanish: ", line
+      #print "Spanish: ", line
       
-      translation = translate(nltk, line)
+      translation = translate(nltk, line, dictionary)
       
       print "English: ", translation
       
-      break
+      #break
 
 
-def translate(nltk, sentence):
+def translate(nltk, sentence, dictionary):
   
   words = sentence.split()
   translated_sentence = ["" for i in range(len(words))]
   pos_words = nltk.spanish_unigram_pos_tag(sentence)
-  
+
   # ["es", "feliz"]
   # ["he is", "happy"]
   
   #go word by word
   #tag each POS
-  #for word_tuple in pos_words:
-    #word, pos = word_tuple
-    #if pos == 'V':
-      # "estan => they are"
-      # do conjugation stuff
+
+  for i, word_tuple in enumerate(pos_words):
+    word, pos = word_tuple
+    if pos == None:
+      stemmed_word = nltk.stem_spanish_word(word)
+      if stemmed_word not in dictionary:
+        matches = difflib.get_close_matches(stemmed_word, dictionary.keys())
+        if len(matches) != 0:
+          stemmed_word = matches[0]
+        else:
+          stemmed_word = word
+      meanings = dictionary[stemmed_word]
+      first_meaning = meanings[0]
+      tokens = first_meaning.split()
+      if len(tokens) > 1 and tokens[0] == "to":
+        pos_words[i] = (word, "V")
+
+  for word_tuple in pos_words:
+    word, pos = word_tuple
+    if pos == None:
+      stemmed_word = ""
+      matches = difflib.get_close_matches(word, dictionary.keys())
+      if len(matches) != 0:
+        stemmed_word = matches[0]
+        meanings = dictionary[stemmed_word]
+        first_meaning = meanings[0]
+        '''
+        tokens = first_meaning.split()
+        if len(tokens) > 1 and tokens[0] == "to":
+          print meanings
+          pos_words[i] = (word, "V")
+        '''
+
+  for i, word_tuple in enumerate(pos_words):
+    word, pos = word_tuple
+    if pos == None:
+      pos_words[i] = (word, "N")
+
+  i = 0
+  while True:
+    word_tuple = pos_words[i]
+    word, pos = word_tuple
+
+    # VERBS
+    if pos == 'V':
+      ser = {"ser":"to be", "soy":"am", "eres":"are", "es":"is", "somos":"are", "son":"are"}
+      ser_past = {"fui":"was", "fuiste":"was", "fue":"was", "fuimos":"were", "fueron":"were"}
+
+      if word in ser:
+        if i < len(pos_words) - 1 and (pos_words[i + 1][0] == "el" or pos_words[i + 1][0] == "la"):
+          translated_sentence[i + 1] = "the"
+          i += 1
+        translated_sentence[i] = ser[word]
+        i += 1
+      elif word in ser_past:
+        if i < len(pos_words) - 1 and (pos_words[i + 1][0] == "el" or pos_words[i + 1][0] == "la"):
+          translated_sentence[i + 1] = "the"
+          i += 1
+        translated_sentence[i] = ser_past[word]
+        i += 1
+
+      else:
+        if "ue" in word[:-1]:
+          word = word.replace("ue", "o")
+        elif "ie" in word[:-1]:
+          word = word.replace("ie", "e")
+        stemmed_word = nltk.stem_spanish_word(word)
+        
+        #print stemmed_word, word
+
+        # if dictionary doesn't have stemmed word, then use closest match from dictionary
+        meaning = ""
+        if stemmed_word not in dictionary:
+          matches = difflib.get_close_matches(stemmed_word, dictionary.keys())
+          if len(matches) != 0:
+            meaning = get_most_likely_definition(nltk, matches[0], dictionary)
+          else:
+            meaing = word
+        else:
+          meaning = get_most_likely_definition(nltk, stemmed_word, dictionary)
+
+        tokens = meaning.split()
+
+        #print word
+        verb_trans = ""
+
+        #VERB TENSES - Regular:
+        # Present tense:
+        # I
+        if stemmed_word + "o" == word:
+          if meaning[:3] == "to ":
+            verb_trans = meaning[3:]
+          else:
+            verb_trans = meaning
+
+        # You
+        elif stemmed_word + "as" == word or stemmed_word + "es" == word:
+          if meaning[:3] == "to ":
+            verb_trans = meaning[3:]
+          else:
+            verb_trans = meaning
+
+        # He/She/It
+        elif stemmed_word + "a" == word or stemmed_word + "e" == word:
+          if meaning[:3] == "to ":
+            verb_trans = meaning[3:] + "s"
+          else:
+            verb_trans = meaning
+
+        # We
+        elif stemmed_word + "amos" == word or stemmed_word + "emos" == word:
+          if meaning[:3] == "to ":
+            verb_trans = meaning[3:]
+          else:
+            verb_trans = meaning
+
+        # They
+        elif (stemmed_word + "an" == word and word[:len(word) - 4] != "aban") or stemmed_word + "en" == word:
+          if meaning[:3] == "to ":
+            verb_trans = meaning[3:]
+          else:
+            verb_trans = meaning
+
+        # Imperfect tense:
+        # I
+        elif stemmed_word + "aba" == word:
+          if meaning[:3] == "to ":
+            verb_trans = meaning[3:] + "ed"
+          else:
+            verb_trans = meaning
+        # You
+
+        elif stemmed_word + "abas" == word:
+          if meaning[:3] == "to ":
+            verb_trans = meaning[3:] + "ed"
+          else:
+            verb_trans = meaning
+
+        # He/She/It
+
+        # We
+
+        # They
+        elif stemmed_word + "aban" == word:
+          if meaning[:3] == "to ":
+            verb_trans = meaning[3:] + "ed"
+          else:
+            verb_trans = meaning
+
+        # PRETERITE
+        # I
+
+
+        # You
+        elif stemmed_word + "aste" == word or stemmed_word + "iste" == word:
+          if meaning[:3] == "to ":
+            verb_trans = meaning[3:] + "ed"
+          else:
+            verb_trans = meaning
+
+        # He/She/It
+
+        # We
+        elif stemmed_word + "amos" == word or stemmed_word + "imos" == word:
+          if meaning[:3] == "to ":
+            verb_trans = meaning[3:] + "ed"
+          else:
+            verb_trans = meaning
+
+        # They
+        elif stemmed_word + "aron" == word or stemmed_word + "ieron" == word:
+          if meaning[:3] == "to ":
+            verb_trans = meaning[3:] + "ed"
+          else:
+            verb_trans = meaning
+
+        else:
+          print stemmed_word, word
+
+        if verb_trans:
+          translated_sentence[i] = verb_trans
+        else:
+          translated_sentence[i] = meaning
+          #print word, translated_sentence[i]
+        i += 1
+
+    # OTHER PARTS OF SPEECH
+    else:
+      i += 1
+
+    if i == len(pos_words):
+      break
+  return translated_sentence
 
   #go word by word
   #if word is a verb, try to figure out tense and subject(1st, 2nd, 3rd person)
@@ -102,7 +291,8 @@ def get_most_likely_definition(nltk, spanish_word, dictionary):
     
   #return best_word
 
-#def get_
+def get_most_likely_definition_bigram(nltk, spanish_word_bigram, dictionary):
+  pass
 
 
 #return "" if the word isn't -ando, -iendo, english word if it is
